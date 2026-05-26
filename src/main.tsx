7572a29c229b29515f34c70a0707e420f79e31fe
landers <liaorenj@gmail.com>
2026-05-26 14:44:13 +0800

    feat: wire terminal color schemes to wterm theme API
    Register all settings palettes as .wterm.theme-* classes and pass theme prop so color scheme changes apply to the wterm renderer.

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import { installBrowserDevMockIfNeeded } from '@/lib/electron-browser-mock'
 import { bootstrapAppFromPreload } from '@/lib/bootstrap-app'
 import App from './App'
 import './index.css'
+import { ensureWtermTerminalThemes } from '@/lib/wterm-theme'
+
+ensureWtermTerminalThemes()
 
 installBrowserDevMockIfNeeded()
 bootstrapAppFromPreload()
